992ce84068084317d24fedb4267ccc2b3ac425b7
Vijay Lakhujani <lakhujanivijay@gmail.com>
2017-06-10 14:05:42 +0530

    Create average_phred_score_per_base.py

---
+++ Quality_score_per_base_per_read/average_phred_score_per_base.py
@@ -0,0 +1,37 @@
+from Bio import SeqIO
+import getopt, sys, re
+
+def usage():
+    print "Usage: base_quality.py -i <input_fastq> -b <position of base as integer> "
+    
+try:
+    options, remainder=getopt.getopt(sys.argv[1:], 'i:b:h')
+
+except getopt.GetoptError as err:
+    print str(err)
+    usage()
+    sys.exit()
+
+for opt, arg in options:
+    if opt in ('-i'):
+        input_file=arg
+    if opt in ('-h'):
+        usage()
+	sys.exit()
+    elif opt in ('-b'):
+        base=arg
+
+pos=int(base)-1
+
+
+for record in SeqIO.parse(input_file, "fastq"):
+    score=record.letter_annotations["phred_quality"]
+    if (int(base) <= len(score)) or (int(base) > 0):
+        print "Mean of " + str(base) + "th base: " 
+        qual.append(score[pos])
+    else:
+        print "\n Error: invalid position of base. Base position should be <= read length and > 0 !\n"
+        sys.exit()
+
+print sum(qual)/len(qual)
+
